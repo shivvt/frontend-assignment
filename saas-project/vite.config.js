fcb03f1a2aaee01e6a3aee30f66850f1e54dd2af
shivvt <shivanshhh@gmail.com>
2024-12-15 17:12:38 +0530

    .

--- saas-project/vite.config.js
+++ saas-project/vite.config.js
@@ -5,8 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   test: {
-    globals: true,   // Enable globals for Jest matchers like 'expect'
+    globals: true, 
     environment: 'jsdom',
-    setupFiles: './src/setupTests.js',  // Path to setupTests.js
+    setupFiles: './src/setupTests.js',
+  },
+  build: {
+    outDir: 'dist',
   },
 });
